802fd74253974afc393efe722c97f56876698828
amarcos <alberto.csm@gmail.com>
2014-02-19 20:42:00 +0000

    Integration Tests UserService test is functional + new test for UserManager (not functional)

--- tests/src/test/java/com.github.pires.example.tests/UserServiceIntegrationTest.java
+++ tests/src/test/java/com.github.pires.example.tests/UserServiceIntegrationTest.java
@@ -5,11 +5,14 @@ import com.github.pires.example.dal.entities.User;
 import io.fabric8.api.Container;
 import io.fabric8.itests.paxexam.support.ContainerBuilder;
 import io.fabric8.itests.paxexam.support.FabricTestSupport;
+import io.fabric8.itests.paxexam.support.SshContainerBuilder;
+import org.apache.karaf.tooling.exam.options.DoNotModifyLogOption;
 import org.fusesource.tooling.testing.pax.exam.karaf.ServiceLocator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.MavenUtils;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.TestProbeBuilder;
 import org.ops4j.pax.exam.junit.Configuration;
@@ -20,10 +23,13 @@ import org.ops4j.pax.exam.spi.reactors.EagerSingleStagedReactorFactory;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 
+import java.io.File;
 import java.util.Set;
 
+import static org.apache.karaf.tooling.exam.options.KarafDistributionOption.*;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.OptionUtils.combine;
 
 
@@ -59,14 +65,9 @@ public class UserServiceIntegrationTest extends FabricTestSupport
         waitForFabricCommands();
 
         System.err.println(executeCommand("features:addUrl mvn:com.github.pires.example/feature-persistence/0.1-SNAPSHOT/xml/features", 10000, false));
-        //System.err.println(executeCommand("features:install swagger", 600000, false));
-//        System.err.println(executeCommand("features:install fabric-cxf", 600000, false));
+        //System.err.println(executeCommand("features:install fabric-cxf/1.0.0.redhat-340", 600000, false));
 
         System.err.println(executeCommand("features:install persistence-aries-hibernate", 600000, false));
-//        System.err.println(executeCommand("features:install transaction", 600000, false));
-//        System.err.println(executeCommand("features:install jpa", 600000, false));
-//        System.err.println(executeCommand("features:install jndi", 600000, false));
-//        System.err.println(executeCommand("features:install application-without-isolation", 600000, false));
 
 
         System.err.println(executeCommand("osgi:install -s mvn:com.github.pires.example/datasource-hsqldb/0.1-SNAPSHOT"));
@@ -74,101 +75,50 @@ public class UserServiceIntegrationTest extends FabricTestSupport
         System.err.println(executeCommand("osgi:install -s mvn:com.github.pires.example/dal-impl/0.1-SNAPSHOT"));
         //System.err.println(executeCommand("osgi:install -s mvn:com.github.pires.example/rest/0.1-SNAPSHOT"));
 
-
-//        System.err.println(executeCommand("fabric:profile-create persistence-example"));
-//        System.err.println(executeCommand("fabric:profile-edit --repositories mvn:com.github.pires.example/feature-persistence/0.1-SNAPSHOT/xml/features persistence-example"));
-//        System.err.println(executeCommand("fabric:profile-edit --features persistence-aries-hibernate persistence-example"));
-//        System.err.println(executeCommand("fabric:profile-edit --bundles mvn:com.github.pires.example/datasource-hsqldb/0.1-SNAPSHOT persistence-example"));
-//        System.err.println(executeCommand("fabric:profile-edit --bundles mvn:com.github.pires.example/dal/0.1-SNAPSHOT persistence-example"));
-//        System.err.println(executeCommand("fabric:profile-edit --bundles mvn:com.github.pires.example/dal-impl/0.1-SNAPSHOT persistence-example"));
-//        System.err.println(executeCommand("fabric:profile-edit --bundles mvn:com.github.pires.example/rest/0.1-SNAPSHOT persistence-example"));
-
         System.err.println("setUp all done");
     }
 
     @After
-    public void tearDown() throws InterruptedException {
-        ContainerBuilder.destroy();
+    public void tearDown() throws InterruptedException
+    {
+        //ContainerBuilder.destroy();
     }
 
     @Configuration
     public Option[] config()
     {
-//        return new Option[]{
-//                // Provision and launch a container based on a distribution of Karaf (Apache ServiceMix).
-//                karafDistributionConfiguration()
-//                       /* .frameworkUrl(
-//                                maven()
-//                                        .groupId("org.fusesource.fabric")
-//                                        .artifactId("fuse-fabric")
-//                                        .version("7.2.0.redhat-024")
-//                                        .type("zip")
-//                        )*/
-//                        .frameworkUrl(
-//                                maven()
-//                                        .groupId("fabric8")
-//                                        .artifactId("fabric8-karaf")
-//                                        .version("1.0.0.redhat-340")
-//                                        .type("zip")
-//                        )
-//                        .karafVersion("2.2.6")
-//                        .name("Fabric Karaf Distro")
-//                        .unpackDirectory(new File("target/pax"))
-//                        .useDeployFolder(false),
-//                // It is really nice if the container sticks around after the test so you can check the contents
-//                // of the data directory when things go wrong.
-//                keepRuntimeFolder(),
-//                // Don't bother with local console output as it just ends up cluttering the logs
-//                configureConsole().ignoreLocalConsole(),
-//                // Force the log level to INFO so we have more details during the test.  It defaults to WARN.
-//                logLevel(LogLevel.INFO),
-//
-//                //load features from feature module
-//                features(maven().groupId("com.github.pires.example").artifactId("feature-persistence").type("xml").classifier("features").version("0.1-SNAPSHOT"), "persistence-aries-hibernate"),
-//                //features(maven().groupId("org.apache.karaf.features").artifactId("standard").type("xml").classifier("features").versionAsInProject(), "http-whiteboard"),
-//                //features(maven().groupId("org.apache.karaf.features").artifactId("enterprise").type("xml").classifier("features").versionAsInProject(), "transaction", "jpa", "jndi"),
-//                //features(maven().groupId("org.apache.cxf.karaf").artifactId("apache-cxf").type("xml").classifier("features").versionAsInProject(), "cxf-jaxws", "cxf", "war"),
-//                //features(maven().groupId("org.apache.activemq").artifactId("activemq-karaf").type("xml").classifier("features").versionAsInProject(), "activemq-blueprint", "activemq-camel"),
-//                //features(maven().groupId("org.apache.camel.karaf").artifactId("apache-camel").type("xml").classifier("features").versionAsInProject(), "camel-blueprint", "camel-jms","camel-jpa", "camel-mvel", "camel-jdbc", "camel-cxf", "camel-test"),
-//
-//                //load all necessary bundles
-//                mavenBundle("com.github.pires.example", "datasource-hsqldb", "0.1-SNAPSHOT"),
-//                mavenBundle("com.github.pires.example", "dal", "0.1-SNAPSHOT"),
-//                mavenBundle("com.github.pires.example", "dal-impl", "0.1-SNAPSHOT"),
-//                //mavenBundle("com.github.pires.example", "rest", "0.1-SNAPSHOT"),
-////                mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-osgi-boot" ).versionAsInProject(),
-////                mavenBundle().groupId( "org.eclipse.jetty.osgi" ).artifactId( "jetty-osgi-boot-jsp" ).versionAsInProject(),
-////
-////                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-websocket" ).versionAsInProject(),
-////                mavenBundle().groupId( "org.eclipse.jetty" ).artifactId( "jetty-servlets" ).versionAsInProject(),
-//
-//
-//                // Remember that the test executes in another process.  If you want to debug it, you need
-//                // to tell Pax Exam to launch that process with debugging enabled.  Launching the test class itself with
-//                // debugging enabled (for example in Eclipse) will not get you the desired results.
-//                //debugConfiguration("5000", true),
-//        };
+       return combine(
+                /*new Option[]{
+                        karafDistributionConfiguration().frameworkUrl(
+                                maven().groupId(GROUP_ID).artifactId(ARTIFACT_ID).version("1.0.0.redhat-346").type("zip")
+                        )
+                                .karafVersion(getKarafVersion()).name("Fabric Karaf Distro").unpackDirectory(new File("target/paxexam/unpack/")),
+                        useOwnExamBundlesStartLevel(50),
+                        envAsSystemProperty(ContainerBuilder.CONTAINER_TYPE_PROPERTY, "child"),
+                        envAsSystemProperty(ContainerBuilder.CONTAINER_NUMBER_PROPERTY, "1"),
+                        envAsSystemProperty(SshContainerBuilder.SSH_HOSTS_PROPERTY),
+                        envAsSystemProperty(SshContainerBuilder.SSH_USERS_PROPERTY),
+                        envAsSystemProperty(SshContainerBuilder.SSH_PASSWORD_PROPERTY),
+                        envAsSystemProperty(SshContainerBuilder.SSH_RESOLVER_PROPERTY),
 
-//        return new Option[]{
-//                new DefaultCompositeOption(fabricDistributionConfiguration()),
-//                mavenBundle("commons-io", "commons-io").versionAsInProject(),
-//                //systemProperty("fabricitest.version").value(System.getProperty("fabricitest.version"))
-//        };
-
-        return combine(
-                fabricDistributionConfiguration()
-                //mavenBundle("org.fusesource.examples", "fabric-cxf-demo-common"),
-                // Passing the system property to the test container
-                //systemProperty("fabricitest.version").value(System.getProperty("fabricitest.version"))
+                        editConfigurationFilePut("etc/config.properties", "karaf.startlevel.bundle", "50"),
+                        editConfigurationFilePut("etc/config.properties", "karaf.startup.message", "Loading Fabric from: ${karaf.home}"),
+                        editConfigurationFilePut("etc/users.properties", "admin", "admin,admin"),
+                        mavenBundle("io.fabric8.itests", "fabric-itests-common", MavenUtils.getArtifactVersion("io.fabric8.itests", "fabric-itests-common")),
+                        mavenBundle("org.fusesource.tooling.testing", "pax-exam-karaf", MavenUtils.getArtifactVersion("org.fusesource.tooling.testing", "pax-exam-karaf")),
+                        new DoNotModifyLogOption(),
+                        keepRuntimeFolder()
+                }*/
+               fabricDistributionConfiguration()
         );
     }
 
     @Test
-    public void shouldCreateAndFindUser_REST() throws Exception
+    public void shouldCreateUserWith_UserService() throws Exception
     {
         Set<Container> containers = ContainerBuilder.create().withName("cnt1").withProfiles("karaf").assertProvisioningResult().build();
 
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         Bundle b = getInstalledBundle("org.hibernate.osgi");
         System.err.println(executeCommand("osgi:restart " + b.getBundleId()));
 
@@ -178,46 +128,54 @@ public class UserServiceIntegrationTest extends FabricTestSupport
         Thread.sleep(2000);
         System.err.println(executeCommand("fabric:cluster-list"));
 
-        Thread.sleep(2000);
-        UserService proxy = ServiceLocator.getOsgiService(UserService.class);
-        assertNotNull(proxy);
-
-        User user = new User();
-        user.setName("alberto");
-        proxy.create(user);
-
-        //Thread.sleep(600000);
-    }
-
-    //    @Test
-    public void shouldCreateAndFindUser_DAL() throws Exception
-    {
-        /*assertTrue(Provision.profileAvailable("persistence-example", "1.0", DEFAULT_TIMEOUT));
-        Set<Container> containers = ContainerBuilder.create().withName("cnt1").withProfiles("default").assertProvisioningResult().build();
         try
         {
-            assertTrue("We should have one container.", containers.size() == 1);
-            System.err.println("created the cxf-server container.");
-            // install bundle of CXF
             Thread.sleep(2000);
-            System.err.println(executeCommand("fabric:cluster-list"));
-            // install bundle of CXF
-            Thread.sleep(2000);
-            // calling the client here
-
-            UserService proxy = ServiceLocator.awaitService(UserService.class);
-            //assertNotNull(proxy);
+            UserService proxy = ServiceLocator.getOsgiService(UserService.class);
+            assertNotNull(proxy);
 
             User user = new User();
             user.setName("alberto");
             proxy.create(user);
-
-            //assertNotSame("We should get the two different result", result1, result2);
         }
         finally
         {
-            ContainerBuilder.destroy();
-        }*/
+            //ContainerBuilder.destroy();
+        }
+
+
+        //Thread.sleep(600000);
+    }
+
+    //@Test
+    public void shouldCreateUserWith_UserManager() throws Exception
+    {
+        Set<Container> containers = ContainerBuilder.create().withName("cnt2").withProfiles("example-quickstarts-rest").assertProvisioningResult().build();
+
+        Thread.sleep(2000);
+        Bundle b = getInstalledBundle("org.hibernate.osgi");
+        System.err.println(executeCommand("osgi:restart " + b.getBundleId()));
+
+        assertTrue("We should have one container.", containers.size() == 1);
+        System.err.println("created the cxf-server container.");
+
+        Thread.sleep(2000);
+        System.err.println(executeCommand("fabric:cluster-list"));
+
+        try
+        {
+            Thread.sleep(2000);
+//            UserService proxy = ServiceLocator.getOsgiService(UserService.class);
+//            assertNotNull(proxy);
+//
+//            User user = new User();
+//            user.setName("alberto");
+//            proxy.create(user);
+        }
+        finally
+        {
+            //ContainerBuilder.destroy();
+        }
     }
 
 
