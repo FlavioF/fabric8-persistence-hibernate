a2f9201d7701577e0a7ead51f6a8f55e48828c91
Alberto <amarcos@bikeemotion.com>
2014-04-03 18:44:23 +0100

    working example with expected error: "java.lang.IllegalArgumentException: Can't convert object of type org.postgresql.util.PGobject"

--- dal.impl/src/test/java/test1.java
+++ dal.impl/src/test/java/test1.java
@@ -1,3 +1,15 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 import com.bikeemotion.common.spatial.GeoPoint;
 import com.github.pires.example.dal.entities.JSON;
 import com.github.pires.example.dal.entities.User;
@@ -82,12 +94,15 @@ public class test1 {
 
     try {
       tx.begin();
-      service.create(value);
+      service.create(value);      
       tx.commit();      
     } catch (Exception ex) {
       if (tx.isActive())
         tx.rollback();
       throw ex;
-    }        
+    }
+
+    int count = service.findAll().size();
+    
   }
 }
